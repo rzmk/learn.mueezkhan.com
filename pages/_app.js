c7f27628587c20e72e1eb015a9afbe5c178c0632
rzmk <mueez25@gmail.com>
2023-02-03 00:53:46 -0500

    :chart_with_upwards_trend: Added Vercel Audiences analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import "../globals.css";
+import { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
